5644b51eb2e6ac71bfd0b624ce52a426849b76cb
Phil Corbett <corbett.phil@gmail.com>
2014-11-29 12:36:38 -0500

    remove unwanted junk!

--- js/app/app.js
+++ js/app/app.js
@@ -5,12 +5,5 @@ App.controller('MainCtrl', ['$scope', function ($scope) {
 }]);
 
 App.controller('RssViewCtrl', ['$scope', function ($scope) {
-  var feeds = ['https://github.com/PureMunky.atom'],
-    i = 0;
 
-  $scope.feedItems = [];
-
-  for (i = 0; i < feeds.length; i++) {
-
-  }
 }]);
