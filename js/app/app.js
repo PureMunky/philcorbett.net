ea2e42b72e86fd69fff357747f178b0f8825ad1c
Phil Corbett <corbett.phil@gmail.com>
2014-11-29 12:25:39 -0500

    add angular to the project

---
+++ js/app/app.js
@@ -0,0 +1,16 @@
+﻿var App = angular.module('App', []);
+
+App.controller('MainCtrl', ['$scope', function ($scope) {
+  
+}]);
+
+App.controller('RssViewCtrl', ['$scope', function ($scope) {
+  var feeds = ['https://github.com/PureMunky.atom'],
+    i = 0;
+
+  $scope.feedItems = [];
+
+  for (i = 0; i < feeds.length; i++) {
+
+  }
+}]);
