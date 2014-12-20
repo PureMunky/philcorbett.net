a41c6c1b4dcea2396dd389a9c1befc4c9aaf8468
Phil Corbett <corbett.phil@gmail.com>
2014-12-20 11:34:24 -0500

    begin comm router stuff

--- js/app/app.js
+++ js/app/app.js
@@ -1,9 +1,30 @@
-﻿var App = angular.module('App', []);
+var App = angular.module('App', ['ngRoute']);
+
+App.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'home.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/_/:commId', {
+        templateUrl: function (arrs) {
+          return 'comm/' + arrs.commId + '.html';
+        },
+        controller: 'CommCtrl',
+        controllerAs: 'comm'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }
+]);
 
 App.controller('MainCtrl', ['$scope', function ($scope) {
-  
+  this.name = 'Main';
 }]);
 
-App.controller('RssViewCtrl', ['$scope', function ($scope) {
-
+App.controller('CommCtrl', ['$scope', '$routeParams', function ($scope, $routeParams) {
+  this.name = $routeParams.commId;
 }]);
