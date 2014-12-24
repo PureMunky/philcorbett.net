502675ef7f49e416f7073f12bb6c04854e5a23f9
Phil Corbett <corbett.phil@gmail.com>
2014-12-24 09:41:05 -0500

    comm controller and structure

--- js/app/app.js
+++ js/app/app.js
@@ -1,16 +1,19 @@
-var App = angular.module('App', ['ngRoute']);
+var App = angular.module('App', ['ngRoute']),
+    config = {
+        partialsPath: 'partials/'
+    };
 
 App.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'home.html',
+        templateUrl: config.partialsPath + 'home.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
       .when('/_/:commId', {
         templateUrl: function (arrs) {
-          return 'comm/' + arrs.commId + '.html';
+          return config.partialsPath + 'comm/index.html';
         },
         controller: 'CommCtrl',
         controllerAs: 'comm'
@@ -26,5 +29,12 @@ App.controller('MainCtrl', ['$scope', function ($scope) {
 }]);
 
 App.controller('CommCtrl', ['$scope', '$routeParams', function ($scope, $routeParams) {
-  this.name = $routeParams.commId;
+  var word = $routeParams.commId;
+
+  $scope.name = word.substr(0,1).toUpperCase() + word.substr(1).toLowerCase();
+  $scope.description = 'hello';
+
+  $scope.getPage = function () {
+    return config.partialsPath + 'comm/words/' + $scope.name + '.html';
+  };
 }]);
