6605225253c72bb90951230801b51997d2233bb2
Phil Corbett <corbett.phil@gmail.com>
2020-07-11 08:54:21 -0400

    Fin writes some javascript.

---
+++ finley.js
@@ -0,0 +1,64 @@
+909996541090 0.3/
+NI"U2
+ ;=
+
+ 
+ ;l>
+(function(){
+    IO ;l //i/; uj/
+     v'6[pk=tkkppppppppppppppppppppppppppppppppp
+    pppppppppppppppppppppppppppppppppppp
+ppppppppppppppppppppppppppppppppppppppppppp
+ppppppppppppppppppppp[[[[[[[[[[[[[[[[[[[
+    [[[[[[[[[[[[[[[[[[[[[[[[[[[
+        [[[[[[[[[bbbbbbbbbbbbbbbbbbbm m/innerHeight./;j
+        bjfhufjblkh,n,bj ByteLengthQueuingStrategyj
+    pppppppppppppppppppppppppppppppppppptl;
+.nljhlu,.y    ]]]]]]]]]]]]]]]]]]]]]]]]]]]
+]]]]]]]]]]]]]]]]]]]]
+    >
+}();'
+/:
+"
+ 0161
+ 5796+79/05
+
+''78.7*19-85720.0.0
+BG/
+y[=u.r] r
+:
+l']|'''';;];                           o\x*
+
+1
+"'
+JFLJ?????????u8loppppppppppo]]]]]]]]]\]]]\
+[p0op;o\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\]-????????????????????????
+??????]=[-0ol??o0-?????
+?????????????
+?????  ['jm m              
+']
+; ;.............o-0o/
+/////////////,,,,,,,,,,,,,,,,,,,,,,,,,,==9
+
+
+       
+  P0-o00oo9k8kliiiiiiiiilliiiuii7--0o90000000i:/
+  ?
+   
+  '/*'
+  'p;
+  l
+   '
+   p 
+   
+    p
+   
+   
+   
+   
+   
+   [89?
+    {}
+    ,l    j 'klk lk' ''\ .;P?????.IO/l?/\0\]\
++06458
+58
